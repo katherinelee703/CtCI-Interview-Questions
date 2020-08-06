61cc4bae3501ace965755f8ff4cc012fd6b97fe3
Katie Durand <khd25@cornell.edu>
2020-08-06 11:44:55 -0500

    example 4.12b

--- 4. Trees & Graphs/4.12b pathsWithSum_II.js
+++ 4. Trees & Graphs/4.12b pathsWithSum_II.js
@@ -6,18 +6,69 @@
 
 Question 4.12b - Paths With Sum II:
 
-You are given a binary tree in which each node contains an integer value (which
-might be positive or negative). Design an algorithm to count the number of paths that sum to a
-given value. The path does not need to start or end at the root or a leaf, but it must go downwards
-(traveling only from parent nodes to child nodes).
-Hints: #6, #14, #52, #68, #77, #87, #94, #103, #108, #115
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+      Note: A leaf is a node with no children.
 
-R(ephrase):
-E(xample):
+
+R(ephrase): find the root-to-leaf paths that add to sum, and list them (as nested array) as the return value
+
+E(xample): 
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \    / \
+7    2  5   1
+Return:
+
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+
 A(pproach):
 C(ode):
 
 */
 
+function pathSum(root, sum) {
+  return recurseTree(root, sum, [], []);
+  // we need to be able to pass 2 "tracking devices"
+  // 1 is to hold the current path (in case it works we will push it to answer array)
+  // 1 is to hold the ongoing additions to the answer array
+}
+
+function recurseTree(root, sum, currentPossiblePath, allPossiblePaths) {
+  if (!root) return allPossiblePaths;
+  // if we get here, there were either answers in the tree, or it will give an [] if there were no paths
+
+  sum -= root.val;
+  currentPossiblePath.push(root.val);
+  // we will never reset this to [] because we can reuse the items alread pushed in as we go "back up" the tree
+
+  if (root.left === null && root.right === null && sum === 0) {
+    allPossiblePaths.push(currentPossiblePath.slice());
+    // we should slice just to be safe, because we are going to keep mutating this array
+    // and we don't want any unexpected things changing as we pass it down and back up
+    // NOTE: you could also copy it this way [...currentPossiblePath] i.e. spread syntax!
+  }
+
+  // now that we've checked where we started, time to start going down the left & right sides of the tree, repeating same steps
+  // repeating same steps means recursion!
+  recurseTree(root.left, sum, currentPossiblePath, allPossiblePaths);
+  recurseTree(root.right, sum, currentPossiblePath, allPossiblePaths);
+
+  // if we get to this line, we have gotten to the bottom of the tree, and need to take off the last leaf item
+  // so we can go up a level and check down on that side if there's anything
+  // if not, it'll pop() and go up again etc..
+  currentPossiblePath.pop();
+
+  return allPossiblePaths;
+}
+
 // T(est):
-// Optimize:
+// Optimize: O(N) because we traverse every node in tree, O(N) recursive stack could take up N space
